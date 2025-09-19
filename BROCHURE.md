e6b81d9fa1aa695b22743fef930cb4eb3fac2b1d
Norm Lorenz <normlorenz@gmail.com>
2025-09-19 07:01:43 -0700

    Finished

--- BROCHURE.md
+++ BROCHURE.md
@@ -1,17 +1,71 @@
-# Columbia Bank Overview
+# Hugging Face Company Brochure
 
-Columbia Bank provides a range of financial services, including personal banking, business banking, commercial banking, and wealth management. Their offerings encompass checking and savings accounts, loans, credit cards, and cash management services for businesses.
+### Overview
+**Hugging Face** is a pioneering platform in the field of machine learning that brings together a vibrant community building the future of artificial intelligence. Our focus is on collaborative efforts where experts and enthusiasts alike can engage with models, datasets, and applications, driving innovation and research in AI.
 
-## Recent Announcements
-- **Name Change**: Umpqua Bank has officially changed its name to Columbia Bank, with no alterations to existing accounts and services.
-- **Merger Completion**: The merger with Pacific Premier Bank is complete, welcoming its clients and providing access to over 350 branches with enhanced business services. However, system transitions will not occur until January 2026.
-- **Business Insights**: The inaugural **Columbia Bank Business Barometer Report** highlights priorities for U.S. businesses in 2025, addressing issues like tariffs and cybersecurity.
+---
 
-## Services Available
-- **Personal Banking**: Options include various checking and savings accounts, home loans, and credit cards.
-- **Business Banking**: Offers checking and savings accounts, loans, and cash management tailored to business needs.
-- **Commercial Banking**: Supports international banking, real estate financing, treasury management, and more.
-- **Wealth Management**: Comprehensive services including financial advising, trust services, and private banking tailored for healthcare professionals.
+### Community-Oriented Culture
+At Hugging Face, we believe in the power of collaboration and open-source principles. We foster a community-driven culture where individuals from diverse backgrounds come together to share knowledge and tools. Our engagement with users and partners showcases our commitment to inclusivity and progress within the AI landscape.
 
-## Online Features
-Columbia Bank provides an array of online and mobile banking tools aimed at enhancing convenience and accessibility for their clients, allowing them to manage their finances, make deposits, and access banking services remotely.
+### What We Offer:
+- **Models**: Access to over **1 million** machine learning models spanning numerous applications and datasets.
+- **Datasets**: Collaboratively created datasets designed to enhance learning and model training, with over **250,000+ datasets** available.
+- **Spaces**: A platform to run applications and models effortlessly, contributing to a seamless user experience.
+
+### Who Uses Hugging Face?
+Hugging Face serves a vast array of clients, from startups to renowned enterprises, including:
+- Google
+- Microsoft
+- Amazon
+- Meta
+These organizations harness our technology to power their AI-driven products and services.
+
+### Careers at Hugging Face
+Joining our team means becoming part of a vibrant and forward-thinking environment. We are actively looking for passionate individuals in various fields, including engineering, product management, and community engagement. 
+- At Hugging Face, you will get opportunities to work with state-of-the-art AI technologies and collaborate with leading experts in the field.
+- We invest in our team members' growth and development, providing a supportive and dynamic work atmosphere where innovation thrives.
+
+---
+
+### Join Us
+Whether you’re a prospective customer looking for cutting-edge AI solutions, an investor interested in the future of technology, or a recruit eager to make a difference, **Hugging Face** is the place where your ambitions can become reality.
+
+**Explore More**: [Visit our website](https://huggingface.co)
+
+---
+
+# Hugging Face Unternehmensbroschüre
+
+### Überblick
+**Hugging Face** ist eine wegweisende Plattform im Bereich des maschinellen Lernens, die eine lebendige Gemeinschaft zusammenbringt, die die Zukunft der künstlichen Intelligenz gestaltet. Unser Schwerpunkt liegt auf der Zusammenarbeit, bei der Fachleute und Interessierte Modelle, Datensätze und Anwendungen gemeinsam nutzen, um Innovation und Forschung im Bereich KI voranzutreiben.
+
+---
+
+### Gemeinschaftsorientierte Kultur
+Bei Hugging Face glauben wir an die Kraft der Zusammenarbeit und der Open-Source-Prinzipien. Wir fördern eine gemeinschaftsgetragene Kultur, in der Menschen aus verschiedenen Hintergründen zusammenkommen, um Wissen und Werkzeuge zu teilen. Unser Engagement mit Nutzern und Partnern zeigt unser Bestreben nach Inklusivität und Fortschritt innerhalb der KI-Landschaft.
+
+### Was Wir Anbieten:
+- **Modelle**: Zugriff auf über **1 Million** Modelle des maschinellen Lernens, die zahlreiche Anwendungen und Datensätze abdecken.
+- **Datensätze**: Kollektiv erstellte Datensätze, die darauf ausgelegt sind, das Lernen und das Training von Modellen zu verbessern, mit über **250.000+ Datensätzen**.
+- **Spaces**: Eine Plattform, um Anwendungen und Modelle mühelos auszuführen und zur nahtlosen Benutzererfahrung beizutragen.
+
+### Wer Hugging Face Nutzt?
+Hugging Face bedient eine Vielzahl von Kunden, von Start-ups bis hin zu renommierten Unternehmen, darunter:
+- Google
+- Microsoft
+- Amazon
+- Meta
+Diese Organisationen nutzen unsere Technologie, um ihre KI-gesteuerten Produkte und Dienstleistungen anzutreiben.
+
+### Karriere bei Hugging Face
+Wenn Sie unser Team beitreten, werden Sie Teil einer dynamischen und zukunftsorientierten Umgebung. Wir suchen aktiv leidenschaftliche Einzelpersonen in verschiedenen Bereichen, darunter Engineering, Produktmanagement und Community-Engagement.
+- Bei Hugging Face haben Sie die Möglichkeit, mit modernster KI-Technologie zu arbeiten und mit führenden Experten auf dem Gebiet zusammenzuarbeiten.
+- Wir investieren in das Wachstum und die Entwicklung unserer Teammitglieder und bieten eine unterstützende und dynamische Arbeitsatmosphäre, in der Innovation gedeiht.
+
+---
+
+### Schließen Sie sich Uns An
+Egal, ob Sie ein potenzieller Kunde sind, der nach hochmodernen KI-Lösungen sucht, ein Investor, der am zukünftigen Technologie interessiert ist, oder ein Rekrut, der etwas bewirken möchte – **Hugging Face** ist der Ort, an dem Ihre Ambitionen Realität werden können.
+
+**Erfahren Sie mehr**: [Besuchen Sie unsere Website](https://huggingface.co)
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from typing import Any, List, Dict
+import json
 import os
+from typing import List, TypedDict
 import requests
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from openai import OpenAI
+
+# Load environment variables in a file called .env
+load_dotenv(override=True)
+
+# Get the OpenAI API key from the environment variable
+api_key = os.getenv('OPENAI_API_KEY')
+
+# Constants
+MODEL = 'gpt-4o-mini'
+
+# Initialize the OpenAI client
+openai = OpenAI()
+
+# Some websites need you to use proper headers when fetching them:
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
 
 
 class Website:
@@ -12,76 +30,131 @@ class Website:
         self.url: str
         self.title: str
         self.text: str
-
-        # Some websites need you to use proper headers when fetching
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
-        }
+        self.body: bytes
 
         self.url = url
         response = requests.get(url, headers=headers)
+        self.body = response.content
         soup = BeautifulSoup(response.content, 'html.parser')
         self.title = soup.title.string if soup.title else "No title found"
-        for irrelevant in soup.body(["script", "style", "img", "input"]):
-            irrelevant.decompose()
-        self.text = soup.body.get_text(separator="\n", strip=True)
+
+        if soup.body:
+            for irrelevant in soup.body(["script", "style", "img", "input"]):
+                irrelevant.decompose()
+            self.text = soup.body.get_text(separator="\n", strip=True)
+        else:
+            self.text = ""
+
+        links = [link.get('href') for link in soup.find_all('a')]
+        self.links = [link for link in links if link]
+
+    def get_contents(self):
+        return f"Webpage Title:\n{self.title}\nWebpage Contents:\n{self.text}\n\n"
 
 
-# Load environment variables in a file called .env
-load_dotenv(override=True)
-
-# Get the OpenAI API key from the environment variable
-api_key = os.getenv('OPENAI_API_KEY')
-
-# Initialize the OpenAI client
-openai = OpenAI()
+class Link(TypedDict):
+    type: str
+    url: str
 
 
 def main() -> None:
     ### High level entry point to run the website summarizer. ###
-    url = input("Enter a URL to summarize: ")
-    display_summary(url)
+    # company_name, url, language = "OpenAI", "https://openai.com", "English"
+    # company_name, url, language = "Anthropic", "https://anthropic.com", "English"
+    # company_name, url, language = "Sullivan Excavating", "https://sullivanexcavatinginc.com", "German"
+    company_name, url, language = "HuggingFace", "https://huggingface.co", "German"
+    # company_name, url, language = "Edward Donner", "https://edwarddonner.com", "English"
+
+    website: Website = Website(url)
+    links: List[Link] = create_links(website)
+    create_brochure(company_name, language, website, links)
 
 
-# Define our system prompt
-system_prompt = "You are an assistant that analyzes the contents of a website \
-and provides a short summary, ignoring text that might be navigation related. \
-Respond in markdown."
+def links_system_prompt() -> str:
+    ### Constructs the system prompt for the OpenAI chat completion API. ###
+    prompt = "You are provided with a list of links found on a webpage. You are able to decide which of the "
+    prompt += "links would be most relevant to include in a brochure about the company, such as links to an "
+    prompt += "About page, or a Company page, or Careers/Jobs pages. \n You should respond in JSON as in this example:"
+    prompt += """
+    {
+        "links": [
+            {"type": "about page", "url": "https://full.url/goes/here/about"},
+            {"type": "careers page", "url": "https://another.full.url/careers"}
+        ]
+    }
+    """
+    return prompt
 
 
-def user_prompt_for(website: Website) -> str:
+def links_user_prompt(website: Website) -> str:
     ### Constructs the user prompt for the OpenAI chat completion API. ###
-    user_prompt = f"You are looking at a website titled {website.title}"
-    user_prompt += "\nThe contents of this website is as follows; \
-please provide a short summary of this website in markdown. \
-If it includes news or announcements, then summarize these too.\n\n"
-    user_prompt += website.text
-    return user_prompt
+    prompt = f"Here is the list of links on the website of {website.url} - "
+    prompt += "please decide which of these are relevant web links for a brochure about the company, respond with "
+    prompt += "the full https URL in JSON format (not markup). Do not include Terms of Service, Privacy, email links. \n"
+    prompt += "Links (some might be relative links):\n"
+    prompt += "\n".join(website.links)
+    return prompt
 
 
-def messages_for(website: Website) -> List[Dict[str, str]]:
-    ###  Constructs the message payload for the OpenAI chat completion API. ###
-    return [
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": user_prompt_for(website)}
-    ]
-
-
-def summarize(url: str) -> str:
-    ### Summarizes the website at the given URL using the OpenAI API. ###
-    website = Website(url)
+def create_links(website: Website) -> List[Link]:
+    ### Creates a list of relevant links for the brochure by using the OpenAI API. ###
     response = openai.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=messages_for(website)
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": links_system_prompt()},
+            {"role": "user", "content": links_user_prompt(website)}
+        ],
     )
-    return response.choices[0].message.content
+    result = response.choices[0].message.content
+
+    try:
+        loads: List[Link] = json.loads(result)
+        return loads
+    except json.JSONDecodeError as e:
+        return {"links": []}
 
 
-def display_summary(url: str) -> None:
-    ###  Writes to a markdown file the summary of the website at the given URL. ###
-    summary = summarize(url)
+def brochure_system_prompt() -> str:
+    ### Constructs the system prompt for the OpenAI chat completion API. ###
+    prompt = "You are an assistant that analyzes the contents of several relevant pages from a company website "
+    prompt += "and creates a short brochure about the company for prospective customers, investors and recruits. "
+    prompt += "Respond in markdown. Include details of company culture, customers and careers/jobs if you have the information."
+    return prompt
+
+
+def brochure_user_prompt(company_name: str, language: str, website: Website, links: List[Link]) -> str:
+    ### Constructs the user prompt for the OpenAI chat completion API. ###
+    prompt = f"You are looking at a company called: {company_name}\n"
+    prompt += f"Here are the contents of its landing page and other relevant pages; use this information "
+    prompt += f"to build a short brochure of the company in markdown. Please translate it into {language}.\n"
+    prompt += get_details(website, links)
+    prompt = prompt[:5_000]  # Truncate if more than 5,000 characters
+    return prompt
+
+
+def get_details(website: Website, links: List[Link]) -> str:
+    ### Fetches and returns the contents of the landing page and other relevant pages. ###
+    result = "Landing page:\n"
+    result += website.get_contents()
+    for link in links["links"]:
+        result += f"\n\n{link['type']}\n"
+        result += Website(link["url"]).get_contents()
+    return result
+
+
+def create_brochure(company_name: str, language: str, website: Website, links: List[Link]) -> None:
+    ### Creates a brochure for the given company by fetching the website and using the OpenAI API. ###
+    response = openai.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": brochure_system_prompt()},
+            {"role": "user", "content": brochure_user_prompt(
+                company_name, language, website, links)}
+        ],
+    )
+    result = response.choices[0].message.content
     with open('BROCHURE.md', 'w', encoding='utf-8') as f:
-        f.write(summary)
+        f.write(result)
 
 
 if __name__ == "__main__":
